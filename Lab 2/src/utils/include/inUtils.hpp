dd2a3d96dd807045ccd7484263bb1114567216df
chemizt <michaelnslinkin@gmail.com>
2019-09-24 21:25:10 +0300

    added new files

---
+++ Lab 2/src/utils/include/inUtils.hpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <iostream>
+
+#include "typeDefs.hpp"
+
+using std::vector;
+using std::cin;
+
+vector<int> createHandFilledVector(uInt size);
+
+intMatrix createHandFilledSquareMatrix(uInt size);
