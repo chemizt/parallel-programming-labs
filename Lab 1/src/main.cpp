8456d279dee1c59cc43a70a2064aaf2beac02a17
Michael Slinkin <michaelnslinkin@gmail.com>
2019-10-17 17:09:56 +0300

    reverted changes to lab 1

--- Lab 1/src/main.cpp
+++ Lab 1/src/main.cpp
@@ -6,10 +6,7 @@ using std::endl;
 
 int main()
 {
-    cout << "Max threads : " << omp_get_max_threads() << "\n";
-    omp_set_num_threads(10);
-
-#pragma omp parallel
+    #pragma omp parallel
     {
         cout << "Hello, World! Thread #" << omp_get_thread_num() << endl;
     }
